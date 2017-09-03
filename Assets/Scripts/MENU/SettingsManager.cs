1fa61adb5ba93953e4e93590c3e6ee9f633da1c5
wickedarc <bernard.larkin@hotmail.com>
2017-09-03 18:38:13 +1000

    Menu visually functional not background functional

---
+++ Assets/Scripts/MENU/SettingsManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour {
+
+
+    public Button UI_Music;
+    public Button UI_SFX;
+    public GameObject ConfirmationPanel;
+
+    bool m_bMuteMusic = false;
+    bool m_bMuteSFX = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void UnMuteMusic()
+    {
+        UI_Music.GetComponentInChildren<Text>().text = "Music:\nMUTE?";
+    }
+    void MuteMusic()
+    {
+        UI_Music.GetComponentInChildren<Text>().text = "Music:\nMUTED";
+    }
+
+    void UnMuteSFX()
+    {
+        UI_SFX.GetComponentInChildren<Text>().text = "SFX:\nMUTE?";
+    }
+    void MuteSFX()
+    {
+        UI_SFX.GetComponentInChildren<Text>().text = "SFX:\nMUTED";
+    }
+
+    public void ResetScores()
+    {
+        ConfirmationPanel.SetActive(true);
+    }
+
+    public void ToggleMusic()
+    {
+        m_bMuteMusic = !m_bMuteMusic;
+
+        if (m_bMuteMusic)
+        {
+            MuteMusic();
+        }
+        else
+        {
+            UnMuteMusic();
+        }
+    }
+
+    public void ToggleSFX()
+    {
+        m_bMuteSFX = !m_bMuteSFX;
+        if (m_bMuteSFX)
+        {
+            MuteSFX();
+        }
+        else
+        {
+            UnMuteSFX();
+        }
+    }
+
+    public void ConfirmationYES()
+    {
+        ConfirmationPanel.SetActive(false);
+    }
+    public void ConfirmationNO()
+    {
+        ConfirmationPanel.SetActive(false);
+    }
+
+
+}
