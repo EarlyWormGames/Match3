b1c57b1e4838b005a388b46807beecf4d0ac320a
wickedarc <bernard.larkin@hotmail.com>
2017-09-03 16:04:35 +1000

    HighScores Complete

---
+++ Assets/Scripts/HighScores/DisplayScore.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayScore : MonoBehaviour {
+
+    GameObject SceneManager;
+    HighScores m_HS;
+    string BuiltString;
+
+    // Use this for initialization
+    void Start () {
+        SceneManager = GameObject.FindGameObjectWithTag("SceneManager");
+        m_HS = SceneManager.GetComponent<HighScores>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void OnGUI()
+    {
+        UpdateText();
+    }
+
+    void UpdateText()
+    {
+        BuiltString = "";
+        Text t = GetComponent<Text>();
+        
+        for (int i = 0; i < m_HS.scores.Length; i++)
+        {
+            BuiltString += m_HS.scores[i].ToString() + "\n";
+        }
+        t.text = BuiltString;
+    }
+}
