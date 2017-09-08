e7050fa967ea9b1a1d1026c032d39877c148c57e
wickedarc <bernard.larkin@hotmail.com>
2017-09-08 20:02:35 +1000

    RT bullShit

---
+++ Assets/Scripts/GUI/Spawn/SpawnObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+
+    GameObject PrefabToSpawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
