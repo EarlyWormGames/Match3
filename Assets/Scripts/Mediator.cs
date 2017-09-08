a80a2fc52516130a71038ecc48ea6b1f07926d7a
Joshua Jones <joshuajones2010@gmail.com>
2017-09-08 16:34:37 +1000

    Added a mediator for passing data between scenes

---
+++ Assets/Scripts/Mediator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings
+{
+    /// <summary>
+    /// How many potential chains will spawn at the start of the game
+    /// </summary>
+    public int RequiredChain = 4;
+
+    /// <summary>
+    /// <para>How likely the game will spawn a new tile based on the most popular colour.</para>
+    /// -1 to make it more likely to spawn different colours
+    /// </summary>
+    public int ColourChance = 1;
+
+    public int GridWidth = 5;
+    public int GridHeight = 7;
+}
+
+public class Mediator
+{
+    public static GameSettings gSettings;
+}
