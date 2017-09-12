597ac25c68f73926953b800dafbc7671fa59ff10
wickedarc <bernard.larkin@hotmail.com>
2017-09-12 10:14:37 +1000

    Working on motion blur

---
+++ Assets/Scripts/GUI/EWDepthOfField.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class EWDepthOfField : MonoBehaviour {
+
+    PostProcessingProfile profile;
+    DepthOfFieldModel Depth;
+
+
+    DepthOfFieldModel.Settings RunTimeSettings;
+
+
+
+    //for setting back the asset file back to its orional state.
+    DepthOfFieldModel.Settings StartSettings;
+
+	// Use this for initialization
+	void Start () {
+        profile = Camera.main.GetComponent<PostProcessingBehaviour>().profile;
+        Depth = profile.depthOfField;
+
+        StartSettings = Depth.settings;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+
+
+
+
+
+    }
+
+
+
+    public void SetFocus(float a_focus)
+    {
+
+    }
+
+    private void OnApplicationQuit()
+    {
+        Depth.settings = StartSettings;
+    }
+
+
+
+
+
+
+}
