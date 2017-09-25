998d278ddae3051db8d12681dd77596c6826b295
wickedarc <bernard.larkin@hotmail.com>
2017-09-25 16:12:16 +1000

    Adding blur effects to the assets folder, working on blur transition

---
+++ Assets/Scripts/Audio/Audio.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour {
+
+    public AudioSource MySource;
+    bool paused = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (paused)
+            {
+                Time.timeScale = 0;
+                MySource.Pause();
+                paused = !paused;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                MySource.UnPause();
+                paused = !paused;
+            }
+        }
+
+
+    }
+}
