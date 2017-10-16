fc2931ee69a334deefe2253d41b55137badbd4b5
wickedarc <bernard.larkin@hotmail.com>
2017-10-17 01:29:49 +1100

    Level Select Settings
    Now using the mediator settings

--- Assets/Scripts/MENU/ChangeScene.cs
+++ Assets/Scripts/MENU/ChangeScene.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class ChangeScene : MonoBehaviour {
-
     
     Fading fade;
 
@@ -21,6 +21,8 @@ public class ChangeScene : MonoBehaviour {
 
     public void ChangeSceneTo(string TargetScene)
     {
+        SetupMediator();
+        
         if (TargetScene == "")
         {
             Debug.LogError("You DUN goofed - change the inspector TargetScene to a scene that exsists in the build settings.");
@@ -30,4 +32,30 @@ public class ChangeScene : MonoBehaviour {
             fade.BeginFade(1, TargetScene);
         }
     }
+
+    void SetupMediator()
+    {
+        GameObject SelectedLevel = GameObject.Find(EventSystem.current.currentSelectedGameObject.name);
+
+        if (SelectedLevel)
+        {
+            LevelSettings LS = SelectedLevel.GetComponent<LevelSettings>();
+            if(Mediator.Settings == null)
+                Mediator.Settings = new GameSettings();
+
+            Mediator.Settings.RequiredChain = LS.RequiredChain;
+            Mediator.Settings.ColourChance = LS.ColourChance;
+            Mediator.Settings.GridWidth = LS.GridWidth;
+            Mediator.Settings.GridHeight = LS.GridHeight + 1;
+            Mediator.Settings.TargetScore = LS.TargetScore;
+            Mediator.Settings.DifficultyMult = LS.DifficultyMult;
+        }
+        else
+        {
+            Debug.LogError("Could not find the current selected level.");
+        }
+
+
+    }
+
 }
---
+++ Assets/Scripts/MENU/LevelSettings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSettings : MonoBehaviour {
+
+    public int RequiredChain = 4;
+
+    public int ColourChance = 1;
+
+    public int GridWidth = 5;
+    public int GridHeight = 7;
+
+    public int TargetScore = 100;
+    public float DifficultyMult = 1;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
