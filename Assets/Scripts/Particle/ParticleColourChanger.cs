f040c3a5afc50e4734a0d3ee4c53e528d88bfdfc
Retr0mancer <mitchell.123@live.com.au>
2017-11-03 17:09:31 +1100

    Fixed a bunch of particle problems, please check if lefty explosion particle works properly

--- Assets/Scripts/Particle/ParticleColourChanger.cs
+++ Assets/Scripts/Particle/ParticleColourChanger.cs
@@ -16,58 +16,58 @@ public class ParticleColourChanger : MonoBehaviour {
     internal ItemColour m_gemColour;
 	// Use this for initialization
 	void Start () {
-        //m_gemParent = GetComponentInParent<NodeItem>();
-        //if(m_gemParent != null)
-        //{
-        //    m_gemColour = m_gemParent.m_Colour;
-        //}
-        //m_childParticles = GetComponentsInChildren<ParticleSystem>();
-        //switch (m_gemColour)
-        //{
-        //    case ItemColour.Capsicum:
-        //        foreach (ParticleSystem ps in m_childParticles)
-        //        {
-        //            ps.GetComponent<ParticleSystemRenderer>().material = CapsicumMat;
-        //        }
-        //        break;
-        //
-        //    case ItemColour.Cake:
-        //        foreach (ParticleSystem ps in m_childParticles)
-        //        {
-        //            ps.GetComponent<ParticleSystemRenderer>().material = CakeMat;
-        //        }
-        //        break;
-        //
-        //    case ItemColour.Broccoli:
-        //        foreach (ParticleSystem ps in m_childParticles)
-        //        {
-        //            ps.GetComponent<ParticleSystemRenderer>().material = BroccoliMat;
-        //        }
-        //        break;
-        //
-        //    case ItemColour.Cupcake:
-        //        foreach (ParticleSystem ps in m_childParticles)
-        //        {
-        //            ps.GetComponent<ParticleSystemRenderer>().material = CupcakeMat;
-        //        }
-        //        break;
-        //
-        //    case ItemColour.Icecream:
-        //        foreach (ParticleSystem ps in m_childParticles)
-        //        {
-        //            ps.GetComponent<ParticleSystemRenderer>().material = IcecreamMat;
-        //        }
-        //        break;
-        //
-        //    case ItemColour.Carrot:
-        //        foreach (ParticleSystem ps in m_childParticles)
-        //        {
-        //            ps.GetComponent<ParticleSystemRenderer>().material = CarrotMat;
-        //        }
-        //        break;
-        //
-        //    default:
-        //        break;
-        //}
-	}
+        m_gemParent = GetComponentInParent<NodeItem>();
+        if (m_gemParent != null)
+        {
+            m_gemColour = m_gemParent.m_Colour;
+        }
+        m_childParticles = GetComponentsInChildren<ParticleSystem>();
+        switch (m_gemColour)
+        {
+            case ItemColour.Capsicum:
+                foreach (ParticleSystem ps in m_childParticles)
+                {
+                    ps.GetComponent<ParticleSystemRenderer>().material = CapsicumMat;
+                }
+                break;
+
+            case ItemColour.Cake:
+                foreach (ParticleSystem ps in m_childParticles)
+                {
+                    ps.GetComponent<ParticleSystemRenderer>().material = CakeMat;
+                }
+                break;
+
+            case ItemColour.Broccoli:
+                foreach (ParticleSystem ps in m_childParticles)
+                {
+                    ps.GetComponent<ParticleSystemRenderer>().material = BroccoliMat;
+                }
+                break;
+
+            case ItemColour.Cupcake:
+                foreach (ParticleSystem ps in m_childParticles)
+                {
+                    ps.GetComponent<ParticleSystemRenderer>().material = CupcakeMat;
+                }
+                break;
+
+            case ItemColour.Icecream:
+                foreach (ParticleSystem ps in m_childParticles)
+                {
+                    ps.GetComponent<ParticleSystemRenderer>().material = IcecreamMat;
+                }
+                break;
+
+            case ItemColour.Carrot:
+                foreach (ParticleSystem ps in m_childParticles)
+                {
+                    ps.GetComponent<ParticleSystemRenderer>().material = CarrotMat;
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
 }
--- Assets/Shaders/SlidingTexture.shader
+++ Assets/Shaders/SlidingTexture.shader
@@ -40,7 +40,7 @@ Shader "Shader Forge/SlidingTexture" {
             #pragma multi_compile _ PIXELSNAP_ON
             #include "UnityCG.cginc"
             #pragma multi_compile_fwdbase
-            #pragma only_renderers d3d9 d3d11 glcore gles 
+            #pragma only_renderers d3d9 d3d11 glcore gles gles3 metal d3d11_9x 
             #pragma target 3.0
             uniform sampler2D _MainTex; uniform float4 _MainTex_ST;
             uniform float4 _Color;
@@ -100,7 +100,7 @@ Shader "Shader Forge/SlidingTexture" {
             #include "Lighting.cginc"
             #pragma fragmentoption ARB_precision_hint_fastest
             #pragma multi_compile_shadowcaster
-            #pragma only_renderers d3d9 d3d11 glcore gles 
+            #pragma only_renderers d3d9 d3d11 glcore gles gles3 metal d3d11_9x 
             #pragma target 3.0
             struct VertexInput {
                 float4 vertex : POSITION;
