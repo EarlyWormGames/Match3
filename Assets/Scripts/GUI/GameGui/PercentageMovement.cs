8b063535970bd312e531bcd19ab4ac83d00f337d
Retr0mancer <mitchell.123@live.com.au>
2017-11-14 15:16:19 +1100

    Health Bar now has particle which plays when the health changes

--- Assets/Scripts/GUI/GameGui/PercentageMovement.cs
+++ Assets/Scripts/GUI/GameGui/PercentageMovement.cs
@@ -10,6 +10,11 @@ public class PercentageMovement : MonoBehaviour
     public float LerpSpeed = 1;
 
     public Image m_Image;
+    public ParticleSystem m_Changer;
+    public Transform BarStart;
+    public Transform BarFinish;
+    public Color GoodColor;
+    public Color BadColor;
 
     GameObject ObjectToMove;
     bool NoErrors = true;
@@ -45,6 +50,12 @@ public class PercentageMovement : MonoBehaviour
 
             //Movement of the Object
             m_Image.fillAmount = Mathf.Lerp(m_Image.fillAmount, Lerp, Time.deltaTime * LerpSpeed);
+
+            float ParticleLerp = Mathf.Lerp(BarStart.position.x, BarFinish.position.x, Percentage);
+            m_Changer.transform.position = new Vector3(Mathf.Lerp(m_Changer.transform.position.x, ParticleLerp, Time.deltaTime * LerpSpeed), m_Changer.transform.position.y, m_Changer.transform.position.z);
+
+            var main = m_Changer.main;
+            main.startColor = Percentage < m_Image.fillAmount ? BadColor : Percentage == m_Image.fillAmount ? new Color(0,0,0,0) : GoodColor;
         }
     }
 }
