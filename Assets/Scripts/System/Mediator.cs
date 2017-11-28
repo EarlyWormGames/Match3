47e36009aa1459ce8c6326e210b4ad97fcbfe569
Joshua Jones <joshuajones2010@gmail.com>
2017-11-28 12:32:05 +1100

    Arcade mode working. Arcade select needs work

--- Assets/Scripts/System/Mediator.cs
+++ Assets/Scripts/System/Mediator.cs
@@ -29,6 +29,7 @@ public class GameSettings
     public int Turns = 15;
     public int Level = -1;
     public bool isArcade = false;
+    public int ArcadeScore = 1000;
 
     public GameObject SpawnObject;
 }
