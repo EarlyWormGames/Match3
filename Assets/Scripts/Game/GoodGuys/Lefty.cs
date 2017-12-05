6510e8439946e207274a63aa61522da8894cff93
Retr0mancer <mitchell.123@live.com.au>
2017-12-05 14:04:33 +1100

    New Line Particle thingy for lefty

--- Assets/Scripts/Game/GoodGuys/Lefty.cs
+++ Assets/Scripts/Game/GoodGuys/Lefty.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Lefty : NodeItem
 {
+    public ParticleSystem m_ConnectorParticle;
     public void Awake()
     {
         
@@ -22,6 +23,15 @@ public class Lefty : NodeItem
             //If it has the same colour as this and isn't already being destroyed
             if (item.m_Shape.m_Colour == m_Colour && !item.m_Shape.MarkDestroy)
             {
+                GameObject ps = Instantiate(m_ConnectorParticle.gameObject, transform.parent);
+                ps.transform.position = transform.position;
+                ps.transform.LookAt(item.transform, Vector3.forward);
+                ps.transform.Rotate(0, -90, 0);
+                var shape = ps.GetComponent<ParticleSystem>().shape;
+                float distance = Vector3.Distance(transform.position, item.transform.position)/2;
+                shape.radius = distance;
+                shape.position = new Vector3(distance, 0, 0);
+                Destroy(ps, 0.4f);
                 //Destroy it!
                 item.StartDestroy();
             }
