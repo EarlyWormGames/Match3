8e5efc5580d3b2dfb9cb2be5dec6706171257eb7
Joshua Jones <joshuajones2010@gmail.com>
2018-01-05 13:22:28 +1100

    Arcade game size changes per level

--- Assets/Scripts/MENU/ArcadeSelect.cs
+++ Assets/Scripts/MENU/ArcadeSelect.cs
@@ -5,11 +5,19 @@ using TMPro;
 
 public class ArcadeSelect : LevelSettings
 {
+    [System.Serializable]
+    public class LevelSize
+    {
+        public int num;
+        public Vector2 size;
+    }
+
     public ChangeScene sceneMan;
     public string scene;
     public TextMeshProUGUI levelDisplay;
     public TextMeshProUGUI scoreDisplay;
     public float TurnsLog = 10;
+    public LevelSize[] levelSizes;
 
     // Use this for initialization
     void Start()
@@ -20,6 +28,16 @@ public class ArcadeSelect : LevelSettings
         scoreDisplay.text = SaveData.ArcadeScore.ToString();
 
         TurnsGoal = (int)(Mathf.Log(LevelNum + 1, TurnsLog) + 0.5f) + 40;
+        DifficultyMult = Mathf.Log(LevelNum + 1, TurnsLog);
+        int i = 0;
+        for (i = 0; i < levelSizes.Length; ++i)
+        {
+            if (levelSizes[i].num > LevelNum)
+                break;
+        }
+
+        GridWidth = (int)levelSizes[i].size.x;
+        GridHeight = (int)levelSizes[i].size.y;
     }
 
     protected override void OnClick()
