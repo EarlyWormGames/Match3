d74377f70bb89b0d30c75dbcc9e88ad7f97a887e
Joshua Jones <joshuajones2010@gmail.com>
2018-01-09 16:43:15 +1100

    Tooth changes sprite
    -Tooth changes sprites based on health %
    -Added a delegate system for the PercentageMovement script
    -Changed Percentage to private and added a public get/set

---
+++ Assets/Scripts/GUI/GameGui/ToothChanger.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToothChanger : MonoBehaviour
+{
+    [System.Serializable]
+    public class Change
+    {
+        public Sprite sprite;
+        public float percentage;
+    }
+
+    public PercentageMovement healthBar;
+    public Image m_Tooth;
+    public ParticleSystem m_PlusParticle;
+    public ParticleSystem m_MinusParticle;
+    public Change[] m_Changes;
+
+    private void OnEnable()
+    {
+        healthBar.OnValueChanged += ValueChanged;
+    }
+
+    private void OnDisable()
+    {
+        healthBar.OnValueChanged -= ValueChanged;
+    }
+
+    void ValueChanged(float prev, float curr)
+    {
+        if (curr > prev)
+        {
+            m_PlusParticle.Play();
+        }
+        else
+        {
+            m_MinusParticle.Play();
+        }
+
+        foreach (var item in m_Changes)
+        {
+            if (curr >= item.percentage)
+            {
+                m_Tooth.sprite = item.sprite;
+            }
+        }
+    }
+}
