471ad5c995674dd905e0bda8388980196d19cd07
Joshua Jones <joshuajones2010@gmail.com>
2018-01-19 15:30:53 +1100

    Backgrounds for game scene. Changes by level num

---
+++ Assets/Scripts/GUI/BG_Change.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BG_Change : MonoBehaviour
+{
+    public Image img;
+    public int[] Levels;
+    public Sprite[] Sprites;
+
+    private void Start()
+    {
+        if (Levels.Length != Sprites.Length)
+        {
+            Debug.LogError("Levels list differs from Sprite list count. Default sprite will be used");
+            return;
+        }
+
+        for (int i = 0; i < Levels.Length; ++i)
+        {
+            if (Mediator.Settings.Level <= Levels[i] || i == Levels.Length - 1)
+            {
+                img.sprite = Sprites[i];
+                break;
+            }
+        }
+    }
+}
