3673f1296dd10c0aa49b3f712125a0f81224cbbb
Joshua Jones <joshuajones2010@gmail.com>
2018-01-25 15:46:07 +1100

    Audio mixer, working volume settings

--- Assets/Scripts/MENU/SettingsManager.cs
+++ Assets/Scripts/MENU/SettingsManager.cs
@@ -2,45 +2,62 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 using TMPro;
 
-public class SettingsManager : MonoBehaviour {
-
-
+public class SettingsManager : MonoBehaviour
+{
     public Button UI_Music;
     public Button UI_SFX;
+    public Slider MusicSlider, SfxSlider;
+
     public GameObject ConfirmationPanel;
+
+    public AudioMixerGroup Master;
 
     bool m_bMuteMusic = false;
     bool m_bMuteSFX = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        float val = 0;
+        Master.audioMixer.GetFloat("MusicMute", out val);
+        if (val < 0)
+            MuteMusic();
+
+        Master.audioMixer.GetFloat("SfxMute", out val);
+        if (val < 0)
+            MuteSFX();
+
+        Master.audioMixer.GetFloat("MusicVol", out val);
+        MusicSlider.value = val;
+
+        Master.audioMixer.GetFloat("SfxVol", out val);
+        SfxSlider.value = val;
+    }
 
     void UnMuteMusic()
     {
         UI_Music.GetComponentInChildren<TextMeshProUGUI>().text = "Music:\nMUTE?";
+        m_bMuteMusic = false;
     }
     void MuteMusic()
     {
         UI_Music.GetComponentInChildren<TextMeshProUGUI>().text = "Music:\nMUTED";
+        m_bMuteMusic = true;
     }
 
     void UnMuteSFX()
     {
         UI_SFX.GetComponentInChildren<TextMeshProUGUI>().text = "SFX:\nMUTE?";
+        m_bMuteSFX = false;
     }
     void MuteSFX()
     {
         UI_SFX.GetComponentInChildren<TextMeshProUGUI>().text = "SFX:\nMUTED";
+        m_bMuteSFX = true;
     }
 
     public void ResetScores()
@@ -50,9 +67,7 @@ public class SettingsManager : MonoBehaviour {
 
     public void ToggleMusic()
     {
-        m_bMuteMusic = !m_bMuteMusic;
-
-        if (m_bMuteMusic)
+        if (!m_bMuteMusic)
         {
             MuteMusic();
         }
@@ -60,12 +75,12 @@ public class SettingsManager : MonoBehaviour {
         {
             UnMuteMusic();
         }
+        MuteItem("MusicMute");
     }
 
     public void ToggleSFX()
     {
-        m_bMuteSFX = !m_bMuteSFX;
-        if (m_bMuteSFX)
+        if (!m_bMuteSFX)
         {
             MuteSFX();
         }
@@ -73,6 +88,7 @@ public class SettingsManager : MonoBehaviour {
         {
             UnMuteSFX();
         }
+        MuteItem("SfxMute");
     }
 
     public void ConfirmationYES()
@@ -92,5 +108,40 @@ public class SettingsManager : MonoBehaviour {
         ConfirmationPanel.SetActive(false);
     }
 
+    public void MuteItem(string group)
+    {
+        float currentval = 0;
+        if (PlayerPrefs.HasKey(group))
+        {
+            currentval = PlayerPrefs.GetFloat(group);
+        }
 
+        if (currentval < 0)
+        {
+            //Toggle sound on
+            Master.audioMixer.SetFloat(group, 0);
+            PlayerPrefs.SetFloat(group, 0);
+        }
+        else
+        {
+            //Toggle sound off
+            Master.audioMixer.SetFloat(group, -80);
+            PlayerPrefs.SetFloat(group, -80);
+        }
+        PlayerPrefs.Save();
+    }
+
+    public void SetMusicVol(float value)
+    {
+        Master.audioMixer.SetFloat("MusicVol", value);
+        PlayerPrefs.SetFloat("MusicVol", value);
+        PlayerPrefs.Save();
+    }
+
+    public void SetSfxVol(float value)
+    {
+        Master.audioMixer.SetFloat("SfxVol", value);
+        PlayerPrefs.SetFloat("SfxVol", value);
+        PlayerPrefs.Save();
+    }
 }
--- Assets/Scripts/SPLASH/StartupData.cs
+++ Assets/Scripts/SPLASH/StartupData.cs
@@ -1,14 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.Analytics;
 
 public class StartupData : MonoBehaviour
 {
+    public AudioMixerGroup Master;
 
     // Use this for initialization
     void Start()
     {
         Analytics.CustomEvent("App Launched");
+
+        if (PlayerPrefs.HasKey("MusicVol"))
+        {
+            Master.audioMixer.SetFloat("MusicVol", PlayerPrefs.GetFloat("MusicVol"));
+        }
+        if (PlayerPrefs.HasKey("SfxVol"))
+        {
+            Master.audioMixer.SetFloat("SfxVol", PlayerPrefs.GetFloat("SfxVol"));
+        }
+
+        if (PlayerPrefs.HasKey("MusicMute"))
+        {
+            Master.audioMixer.SetFloat("MusicMute", PlayerPrefs.GetFloat("MusicMute"));
+        }
+        if (PlayerPrefs.HasKey("SfxMute"))
+        {
+            Master.audioMixer.SetFloat("SfxMute", PlayerPrefs.GetFloat("SfxMute"));
+        }
     }
 }
