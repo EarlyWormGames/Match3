ba7d1acb7f430674471f52c705b0bc33281e2801
Joshua Jones <joshuajones2010@gmail.com>
2018-02-11 00:14:42 +1100

    Adding Facebook sdk stuff

---
+++ Assets/Scripts/Coroutiner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutiner : MonoBehaviour
+{
+    private static List<IEnumerator> routines = new List<IEnumerator>();
+    private static Coroutiner instance;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (routines.Count > 0)
+        {
+            IEnumerator[] frameRoutines = routines.ToArray();
+            foreach (var item in frameRoutines)
+            {
+                StartCoroutine(item);
+                routines.RemoveAt(0);
+            }
+        }
+    }
+
+    public static void BeginCoroutine(IEnumerator routine)
+    {
+        routines.Add(routine);
+    }
+}
---
+++ Assets/Scripts/Facebook/GameStateManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameStateManager : MonoBehaviour
+{
+    public static string Username;
+    public static Texture UserTexture;
+    public static int Score, HighScore;
+    public static Dictionary<string, Texture> FriendImages = new Dictionary<string, Texture>();
+    public static List<object> Scores = new List<object>();
+    public static List<object> Friends = new List<object>();
+    public static string FriendID;
+    public static string ServerURL;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static void CallUIRedraw()
+    {
+
+    }
+
+    public static void ShowPopup(string text, UnityAction action = null)
+    {
+
+    }
+}
